3b8ec9a69c0b569ee608ccd233a42eb8bd18f925
Võ Thành Tài <taivtse@gmail.com>
2019-02-11 13:49:40 +0700

    Refactor a bit code

--- src/main/java/vn/edu/fpt/controller/DepartController.java
+++ src/main/java/vn/edu/fpt/controller/DepartController.java
@@ -4,7 +4,6 @@ import org.hibernate.StaleStateException;
 import org.hibernate.exception.ConstraintViolationException;
 import org.hibernate.exception.DataException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
