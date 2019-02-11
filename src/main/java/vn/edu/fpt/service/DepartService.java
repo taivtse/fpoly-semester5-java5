5eb76700d049be306122ae6fab1034108ddc0978
Võ Thành Tài <taivtse@gmail.com>
2019-02-11 17:38:06 +0700

    Set active state when modify data

--- src/main/java/vn/edu/fpt/service/DepartService.java
+++ src/main/java/vn/edu/fpt/service/DepartService.java
@@ -4,4 +4,5 @@ import vn.edu.fpt.dto.DepartDto;
 import vn.edu.fpt.service.generic.GenericService;
 
 public interface DepartService extends GenericService<String, DepartDto> {
+
 }
--- src/main/java/vn/edu/fpt/service/impl/DepartServiceImpl.java
+++ src/main/java/vn/edu/fpt/service/impl/DepartServiceImpl.java
@@ -1,11 +1,13 @@
 package vn.edu.fpt.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import vn.edu.fpt.dao.DepartDao;
 import vn.edu.fpt.dao.generic.GenericDao;
 import vn.edu.fpt.dto.DepartDto;
+import vn.edu.fpt.entity.DepartEntity;
 import vn.edu.fpt.mapper.DepartMapper;
 import vn.edu.fpt.service.DepartService;
 import vn.edu.fpt.service.generic.GenericServiceImpl;
@@ -13,10 +15,41 @@ import vn.edu.fpt.service.generic.GenericServiceImpl;
 @Service
 @Transactional
 public class DepartServiceImpl extends GenericServiceImpl<String, DepartDto, DepartMapper> implements DepartService {
+
+    @Autowired
+    private DepartMapper mapper;
+
     private DepartDao departDao;
 
     public DepartServiceImpl(@Qualifier("departDaoImpl") GenericDao genericDao) {
         super(genericDao);
         this.departDao = (DepartDao) genericDao;
+    }
+
+    @Override
+    public void save(DepartDto dto) throws Exception {
+//        processing before save
+        dto.setActive(true);
+
+        DepartEntity entity = mapper.dtoToEntity(dto);
+        departDao.save(entity);
+    }
+
+    @Override
+    public void update(DepartDto dto) throws Exception {
+//        processing before update
+        dto.setActive(true);
+
+        DepartEntity entity = mapper.dtoToEntity(dto);
+        departDao.update(entity);
+    }
+
+    @Override
+    public void delete(DepartDto dto) throws Exception {
+//        mark as deleted
+        dto.setActive(false);
+
+        DepartEntity entity = mapper.dtoToEntity(dto);
+        departDao.update(entity);
     }
 }
