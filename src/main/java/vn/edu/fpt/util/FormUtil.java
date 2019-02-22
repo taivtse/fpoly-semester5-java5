f4955e521be7c4576b9594b3dc3e6e30b7e09410
Võ Thành Tài <taivtse@gmail.com>
2019-02-22 16:06:19 +0700

    Inject localResolver to get cookie local and set to command

--- src/main/java/vn/edu/fpt/util/FormUtil.java
+++ src/main/java/vn/edu/fpt/util/FormUtil.java
@@ -1,15 +1,33 @@
 package vn.edu.fpt.util;
 
-import org.apache.commons.beanutils.*;
+import org.apache.commons.beanutils.BeanUtilsBean;
+import org.apache.commons.beanutils.ConvertUtilsBean;
+import org.apache.commons.beanutils.PropertyUtilsBean;
 import org.apache.commons.beanutils.converters.DateConverter;
 import org.apache.commons.beanutils.converters.DateTimeConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import vn.edu.fpt.command.AbstractCommand;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Date;
 
+@Component
 public class FormUtil {
-    public static <T> T populate(Class<T> clazz, HttpServletRequest request) {
+    private static CookieLocaleResolver localeResolver;
+
+    @Autowired
+    private CookieLocaleResolver injectedLocaleResolver;
+
+    @PostConstruct
+    public void init() {
+        localeResolver = injectedLocaleResolver;
+    }
+
+    public static <T extends AbstractCommand> T populate(Class<T> clazz, HttpServletRequest request) {
         T object = null;
         try {
             object = clazz.newInstance();
@@ -22,6 +40,8 @@ public class FormUtil {
 
             BeanUtilsBean beanUtilsBean = new BeanUtilsBean(convertUtilsBean, new PropertyUtilsBean());
             beanUtilsBean.populate(object, request.getParameterMap());
+
+            object.setLocale(localeResolver.resolveLocale(request));
         } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
             e.printStackTrace();
         }
